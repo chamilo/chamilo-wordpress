779733cb22157eeba1b8970cabe249244d182931
Yannick Warnier <ywarnier@beeznest.org>
2016-07-11 20:41:58 -0500

    Add starting script and basic info about the plugin

---
+++ chamilo.php
@@ -0,0 +1,10 @@
+<?php
+/*
+Plugin Name: Chamilo
+Description: Connect to your Chamilo portal through simple settings
+Version: 1.0
+Author: Yannick Warnier
+Author URI: https://github.com/ywarnier
+License: GPLv3 or later
+License URI: https://www.gnu.org/licenses/gpl-3.0.html
+*/
