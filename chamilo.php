42d157a7dfb398d4f5d161d358759d82c71e5e45
Yannick Warnier <ywarnier@beeznest.org>
2016-07-16 11:55:54 -0500

    Add Chamilo SOAP code + rename settings directory

--- chamilo.php
+++ chamilo.php
@@ -7,6 +7,7 @@ Author: Yannick Warnier
 Author URI: https://github.com/ywarnier
 License: GPLv3 or later
 License URI: https://www.gnu.org/licenses/gpl-3.0.html
+Text Domain: chamilo
 
 {Plugin Name} is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
@@ -22,7 +23,7 @@ any later version.
   along with {Plugin Name}. If not, see {License URI}.
 */
 
-include __DIR__ . '/settings/chamilo-functions.php';
+include __DIR__ . '/includes/chamilo-functions.php';
 // If this file is called directly, abort.
 if ( ! defined( 'WPINC' ) ) {
         die;
@@ -38,3 +39,5 @@ load_plugin_textdomain('chamilo', false, basename( dirname( __FILE__) ) . '/lang
 add_action( 'admin_init', 'chamilo_settings_api_init' );
 //add_action( 'admin_menu', 'chamilo_menu' );
 //add_menu_page( 'options-general.php', __( 'ChamiloSettings' ), __( 'ChamiloSettings' ), 'manage_options', 'chamilo-submenu-handle', 'chamilo_menu' );
+
+add_shortcode( 'chamilo_courses_list', 'chamilo_get_courses' );
---
+++ includes/chamilo-functions.php
@@ -0,0 +1,151 @@
+<?php
+/**
+ * Define a few constants
+ */
+define('CHAMILO_WP_PUBLIC_IP', '');
+/**
+ * Basic install/uninstall functions
+ */
+function chamilo_install() {
+    // Código de instalación
+    register_setting( 'reading', 'chamilo_setting_url' );
+    register_setting( 'reading', 'chamilo_setting_key' );
+}
+function chamilo_deactivation() {
+    // Código de desactivación
+}
+function chamilo_uninstall() {
+    // Código de desinstalación
+    unregister_setting( 'chamilo_setting_url' );
+    unregister_setting( 'chamilo_setting_key' );
+}
+
+/**
+ * Basic settings functions
+ */
+function chamilo_connectivity_section_callback() {
+    echo '<p>' . __( 'Please configure your global Chamilo connectivity settings', 'chamilo' ) . '</p>';
+}
+
+function chamilo_setting_url_callback_function() {
+    $setting = esc_attr( get_option( 'chamilo_setting_url' ) );
+    echo "<input type='text' name='chamilo_setting_url' value='$setting' />";
+}
+
+function chamilo_setting_key_callback_function() {
+    $setting = esc_attr( get_option( 'chamilo_setting_key' ) );
+    echo "<input type='text' name='chamilo_setting_key' value='$setting' />";
+}
+
+function chamilo_settings_api_init() {
+    add_settings_section(
+        'chamilo_connectivity_section',
+        __( 'Chamilo connectivity', 'chamilo' ),
+        'chamilo_connectivity_section_callback',
+        'reading'
+    );
+    add_settings_field(
+        'chamilo_setting_url',
+        __( 'Chamilo\'s portal url', 'chamilo' ),
+        'chamilo_setting_url_callback_function',
+        'reading',
+        'chamilo_connectivity_section'
+    );
+    register_setting('reading', 'chamilo_setting_url');
+    add_settings_field(
+        'chamilo_setting_key',
+        __( 'Chamilo\'s security key', 'chamilo' ),
+        'chamilo_setting_key_callback_function',
+        'reading',
+        'chamilo_connectivity_section'
+    );
+    register_setting('reading', 'chamilo_setting_key');
+}
+
+/**
+ * Basic menu functions
+ */
+
+/**
+ * Get data from Chamilo
+ */
+function chamilo_get_courses($visibility) {
+    $signature = chamilo_get_signature();
+    $visibilities = array('public', 'public-registered');
+    error_log('Calling webservice');
+    $courses = chamilo_soap_call( 'courses_list', 'WSCourseList', $signature, $visibilities );
+    var_dump($courses);
+    error_log(print_r($courses, 1));
+}
+
+function chamilo_soap_call() {
+    // Prepare params
+    $params = func_get_args();
+    $service = array_shift($params);
+    $action = array_shift($params);
+    ini_set('soap.wsdl_cache_enabled', 0);
+    $services = array( 'courses_list', 'user_info', 'registration' );
+    if ( !in_array( $service, $services ) ) {
+        // Asking for rogue service, blocking!
+        return false;
+    }
+
+    $service_path = get_option('chamilo_setting_url');
+    if (substr($service_path, -1, 1) != '/') {
+        $service_path .= '/';
+    }
+    $service_path .= 'main/webservices/' . $service . '.soap.php?wsdl';
+
+    // Init SOAP client
+    if (!empty($service_path)) {
+        $client = new SoapClient($service_path);
+        // Make call and its return result
+        try {
+            $r = $client->__soapCall($action, $params);
+        } catch (Exception $e) {
+            error_log('In chamilo_soap_call, exception when calling: '.$e->getMessage());
+            return false;
+        }
+        return $r;
+    } else {
+        return FALSE;
+    }
+}
+
+function chamilo_get_signature() {
+    global $user;
+    
+    switch ($type) {
+        case CHAMILO_PERUSER_SIGNATURE:
+            //chamilo_load_user_data($user);
+            //if (isset($user->chamilo_settings)) {
+            //    return sha1($user->chamilo_settings['user'] . $user->chamilo_settings['apikey']);
+            //}
+            return '';
+            break;
+        case CHAMILO_SECRET_KEY:
+            $addr = (CHAMILO_WP_PUBLIC_IP == '' ? $_SERVER['SERVER_ADDR'] : CHAMILO_WP_PUBLIC_IP);
+            $chamilo_apikey = sha1( $addr . get_option( 'chamilo_setting_key' ) );
+            return $chamilo_apikey;
+            break;
+        case CHAMILO_GLOBAL_SIGNATURE:
+        default:
+            //$chamilo_user = variable_get('chamilo_user', '');
+            //$chamilo_apikey = variable_get('chamilo_appkey', '');
+            //return sha1($chamilo_user . $chamilo_apikey);
+            return '';
+    }
+}
+
+function chamilo_get_course_visibilities() {
+    return array(
+        'public' => __('public', 'chamilo'),
+        'private' => __('private', 'chamilo'),
+        'public-registered' => __('public registered', 'chamilo'),
+        'closed' => __('closed', 'chamilo')
+    );
+}
+
+/**
+ * Add blocks
+ */
